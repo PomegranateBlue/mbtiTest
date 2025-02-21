4a6284d774435fd471f643344d225237b7aef40b
bluepome98 <wpdn1998@gmail.com>
2025-02-21 19:29:42 +0900

    회원가입 기능 구현 - 초기

--- src/components/SignUp.jsx
+++ src/components/SignUp.jsx
@@ -1,11 +1,53 @@
+import { useState } from "react";
+import { useNavigate } from "react-router-dom";
+import { register } from "../api/auth";
 const SignUp = () => {
+  const [id, setId] = useState("");
+  const [password, setPassword] = useState("");
+  const [nickname, setNickname] = useState("");
+  const navigate = useNavigate();
+
+  const handleSignUp = async (e) => {
+    e.preventDefault();
+    const registerData = { id, password, nickname };
+
+    try {
+      const response = await register(registerData);
+      if (response.success) {
+        alert("회원가입 성공, 로그인해주세요");
+        navigate("/login");
+      }
+    } catch (error) {
+      alert(error);
+    }
+  };
   return (
     <div>
-      <form>
-        <input type="text" placeholder="아이디" required />
-        <input type="text" placeholder="비밀번호" required />
-        <input type="text" placeholder="닉네임" required></input>
-        <button>가입하기</button>
+      <form onSubmit={handleSignUp}>
+        <input
+          type="text"
+          placeholder="아이디"
+          value={id}
+          onChange={(e) => setId(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="비밀번호"
+          value={password}
+          onChange={(e) => setPassword(e.target.value)}
+          required
+        />
+        <input
+          type="text"
+          placeholder="닉네임"
+          value={nickname}
+          onChange={(e) => {
+            setNickname(e.target.value);
+          }}
+          required
+        ></input>
+        <button type="submit">가입하기</button>
       </form>
     </div>
   );
