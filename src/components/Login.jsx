b2fe0721eb37574b252d68999c7a8013609810e0
bluepome98 <wpdn1998@gmail.com>
2025-02-21 19:15:00 +0900

    눈 아프니까 index.css에서 전체 배경 설정

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,6 +1,6 @@
 const LogIn = () => {
   return (
-    <div className="flex">
+    <div className="flex bg-slate-700 justify-center p-8 gap-10">
       <input type="text" placeholder="아이디" required />
       <input type="text" placeholder="비밀번호" required />
       <button>로그인</button>
